97cb46613bbbf0c3cb0e4b59c95c456ced28fc86
APex <37938428+APexBlockchainDevelopments@users.noreply.github.com>
2024-05-29 23:50:21 -0600

    5-29

--- 6-Selfie/src/SimpleGovernance.sol
+++ 6-Selfie/src/SimpleGovernance.sol
@@ -108,4 +108,6 @@ contract SimpleGovernance is ISimpleGovernance {
         uint256 halfTotalSupply = _governanceToken.getTotalSupplyAtLastSnapshot() / 2;
         return balance > halfTotalSupply;
     }
+
+    
 }
